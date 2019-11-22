ad0da0744798bb7baa4329a1cdfefe14a39d811a
Youssouf SAMEUT BOUHAIK <youssouf.sameut_bouhaik@hotmail.com>
2019-11-22 16:44:48 +0100

    added update script

---
+++ update.sh
@@ -0,0 +1,6 @@
+cp config.fish ~/.config/fish/
+cp init.vim ~/.config/nvim/
+cp config ~/.config/regolith/i3/
+sudo cp weather /usr/share/i3blocks/
+sudo cp i3blocks.conf /etc/
+sudo cp compton.conf /etc/xdg/
